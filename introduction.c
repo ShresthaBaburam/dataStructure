606f81d7bd3a7c245d36ebff8665d56f64e9ed8e
ShresthaBaburam <diploveshikha@gamil.com>
2021-01-08 12:21:39 +0545

    the basic or the overview of the data structure

---
+++ introduction.c
@@ -0,0 +1,157 @@
+/*
+Data Structure is a way to store and organize data so that it can be used efficiently
+The data structure name indicates itself that organizing the data in memory. 
+There are many ways of organizing the data in the memory 
+
+Types of Data Structures:
+
+    Primitive data structure
+        The primitive data structures are primitive data types. The int, char, float,
+        double, and pointer are the primitive data structures that can hold a single value.
+
+    Non-primitive data structure
+
+        Linear Data Structure
+            The arrangement of data in a sequential manner is known as a linear data structure. 
+            The data structures used for this purpose are Arrays, Linked list, Stacks, and Queues.
+            One element is connected to only one another element in a linear form. 
+
+            Static data structure: 
+                It is a type of data structure where the size is allocated at the compile time. 
+                Therefore, the maximum size is fixed.
+                Arrays
+
+            Dynamic data structure: 
+                It is a type of data structure where the size is allocated at the run time. 
+                Therefore, the maximum size is flexible.
+                Stack, Queues, Linked List
+
+        Non-linear data structure. 
+            When one element is connected to the 'n' number of elements
+            The best example is trees and graphs. 
+            The elements are arranged in a random manner.
+
+Major Operations
+
+    Traversing: 
+        Every data structure contains the set of data elements. 
+        Traversing the data structure means visiting each element of the 
+        data structure in order to perform some specific operation like searching or sorting.
+
+    Searching: 
+        We can search for any element in a data structure.
+
+    Sorting: 
+        We can sort the elements of a data structure either in an ascending or descending order.
+    
+    Insertion: 
+        We can also insert the new element in a data structure.
+    
+    Updation: 
+        We can also update the element, i.e., we can replace the element with another element.
+    
+    Deletion: 
+        We can also perform the delete operation to remove the element from the data structure.
+    Merging: 
+        When two lists List A and List B of size M and N respectively, 
+        of similar type of elements, clubbed or joined to produce the third list,
+        List C of size (M+N), then this process is called merging
+
+Advantages of Data structures
+
+    Efficiency: 
+        If the choice of a data structure for implementing a particular ADT is proper, 
+        it makes the program very efficient in terms of time and space.
+    
+    Reusability: 
+        The data structures provide reusability means that multiple client 
+        programs can use the data structure.
+    
+    Abstraction: 
+        The data structure specified by an ADT also provides the level of abstraction. 
+        The client cannot see the internal working of the data structure, 
+        so it does not have to worry about the implementation part. 
+        The client can only see the interface.
+*/
+
+/*
+Prerequisite:
+    Array
+    Structure
+    Union
+    Class
+    Pointer
+
+Stack:
+    Primitives Operations:
+        Push()
+        Pop()
+    Stack as ADT
+    Stack Opeartions
+    Evaluation of Prefix, Infix and Postfix Expression
+    Expression Evaluation
+
+Queue
+    Primitives Operations:
+        Enqueue()
+        Dequeue()
+    Queue as ADT
+    Queue Opeartions
+    Linear Queue
+    CIrcular Queue
+    Double Ended Queue
+    Prioriy Queue
+
+List
+    Array Implementation of List
+    Queues as List
+    List Operations
+
+Linked List
+    Dynamic Implementation
+    Linked List as an ADT
+    Linked list Operations
+    Doubly Linked List
+    Linked Implemettations of Stacks and Queues
+
+Recursion
+    Principle and Comparision between Recursion and Iteration
+    Factorial, Fibonacci......
+    TOH
+    Validity of Expressons
+
+Tree
+    Binary Tree
+    Operations in Binary Tree
+        Insert()
+        Delete()
+    Binary Tree Traversals
+        Preorder
+        Inorder
+        Postorder
+    Balanced Tree
+    AVL Tree
+    Game Tree
+    B-Tree
+    
+Sorting
+    Internal and Enternal Sort
+    Insertion and Selection Sort
+    Exchange Sort
+    Bubble and Quick Sort
+    Merge and Radix Sort
+    Shell Sort
+    Heap Sort as Priority Queue
+    Efficiency of Sorting
+
+Searching
+    Sequential Search
+    Binary Search
+    Hashing
+
+Graph
+    Representations
+    Graphs as ADT
+    
+
+*/
